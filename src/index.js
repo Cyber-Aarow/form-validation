fbfefc52b79f6418345b5f5956133f055abe7f69
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2025-12-19 16:59:46 -0800

    Make the error messages show by adding validation functions for each input and an overseer function to manage the validations with a map.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import './main.css';
 
-const countrySelect = document.querySelector('#country');
-const postalCodeField = document.querySelector('#postal_code');
 const form = document.querySelector('#form');
 
-function checkPostalCode(){
+const email = document.querySelector('#email');
+const postalCode = document.querySelector('#postal_code');
+const countrySelect = document.querySelector('#country');
+const confirmPassword = document.querySelector('#confirm_password');
+
+
+
+function showError(input, message){
+    const error = document.getElementById(input.dataset.error);
+    error.textContent = message;
+    if(message !== ""){
+        error.classList.add('active');
+    }
+    else{
+        error.classList.remove('active');
+    }
+}
+
+function validateEmail(input){
+    showError(input, input.validationMessage);
+}
+
+function validatePostalCode(input){
     const constraints = {
         ch: [
             "^(CH-)?\\d{4}$",
@@ -19,22 +39,52 @@ function checkPostalCode(){
     const country = countrySelect.value;
 
     if(!constraints[country]){
-        postalCodeField.setCustomValidity("Please select a country.");
+        input.setCustomValidity("Please select a country.");
         return;
     }
 
     const constraint = new RegExp(constraints[country][0], "");
 
-    if (constraint.test(postalCodeField.value)){
-        postalCodeField.setCustomValidity("");
+    if (constraint.test(input.value)){
+        input.setCustomValidity("");
     }
     else{
-        postalCodeField.setCustomValidity(constraints[country][1]);
+        input.setCustomValidity(constraints[country][1]);
     }
+    showError(input, input.validationMessage);
 }
 
-countrySelect.addEventListener('change', checkPostalCode);
-postalCodeField.addEventListener('input', checkPostalCode);
+function validateConfirmPassword(input){
+    const password = document.querySelector('#password');
+    if(password.value !== input.value){
+        input.setCustomValidity("The passwords do not match.");
+    }
+    else{
+        input.setCustomValidity("");
+    }
+    showError(input, input.validationMessage);
+}
+
+const validators = {
+    email: validateEmail,
+    postal_code: validatePostalCode,
+    confirm_password: validateConfirmPassword,
+}
+
+function validateField(input){
+    validators[input.id]?.(input);
+}
+
+
+
+email.addEventListener('input', (e) => validateField(e.target));
+postalCode.addEventListener('input', (e) => validateField(e.target));
+confirmPassword.addEventListener('input', (e) => validateField(e.target));
+//Country affects postal_code validity
+countrySelect.addEventListener('change', () => validateField(postalCode));
+
+
+
 form.addEventListener('submit', (e)=>{
     if(!form.checkValidity()){
         e.preventDefault();
